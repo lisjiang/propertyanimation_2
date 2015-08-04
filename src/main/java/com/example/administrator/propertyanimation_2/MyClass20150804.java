de04c20e0a112d70577332900b3ae89ed88ca9ed
lisjiang <agnesjiang@qq.com>
2015-08-04 14:14:38 +0800

    new class

---
+++ src/main/java/com/example/administrator/propertyanimation_2/MyClass20150804.java
@@ -0,0 +1,11 @@
+package com.example.administrator.propertyanimation_2;
+
+/**
+ * Created by Bin(agnesjiang@qq.com) on 2015/8/4.
+ */
+public class MyClass20150804 implements Runnable {
+    @Override
+    public void run() {
+
+    }
+}
